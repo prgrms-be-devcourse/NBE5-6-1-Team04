2e2bc9e2079066c9dd764c01a0f90b67942224b3
강현진 <66511872+Juferis@users.noreply.github.com>
2025-04-25 22:25:20 +0900

    feat: 관리자 페이지 생성 및 주문 목록 바인딩

---
+++ src/main/java/com/grepp/spring/app/controller/web/adminpage/adminPageController.java
@@ -0,0 +1,24 @@
+package com.grepp.spring.app.controller.web.adminpage;
+
+import com.grepp.spring.app.model.order.OrderService;
+import com.grepp.spring.app.model.order.dto.OrderDto;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class adminPageController {
+
+    private final OrderService orderService;
+
+    @GetMapping("/admin/orders")
+    public String showAllOrders(Model model) {
+        List<OrderDto> Orders = orderService.getAllOrders();
+        model.addAttribute("orders", Orders);
+        return "admin/admin-page";
+    }
+
+}
