e70e32ec2441c02af14c8ad6df3a1edf61a924c9
강현진 <66511872+Juferis@users.noreply.github.com>
2025-04-26 22:43:29 +0900

    feat: cart 스크립트 수정 및 로컬스토리지 이용

--- src/main/webapp/assets/js/cart.js
+++ src/main/webapp/assets/js/cart.js
@@ -1,32 +1,51 @@
-const cart = {};
+const LOCALSTORAGE_KEY = 'cart';
 
-function addToCart(productId, productCount) {
+function getCart() {
+  const savedCart = localStorage.getItem(LOCALSTORAGE_KEY);
+  return savedCart ? JSON.parse(savedCart) : {};
+}
+
+function updateCart(cart) {
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(cart));
+}
+
+function clearCart() {
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function addToCart(productId, productName, productPrice, productCount) {
+  const cart = getCart();
   const count = parseInt(productCount, 10);
 
   if (!cart[productId]) {
     cart[productId] = {
       productId,
-      quantity: count,
+      productName,
+      productPrice,
+      productCount: count,
     };
   } else {
-    cart[productId].quantity += count;
+    cart[productId].productCount += count;
   }
 
+  updateCart(cart);
   console.log('현재 장바구니:', cart);
 }
 
 function removeToCart(productId, productCount) {
+  const cart = getCart();
   if (!cart[productId]) {
     return;
   }
 
   const count = parseInt(productCount, 10);
 
-  cart[productId].quantity -= count;
+  cart[productId].productCount -= count;
 
-  if (cart[productId].quantity <= 0) {
+  if (cart[productId].productCount <= 0) {
     delete cart[productId];
   }
 
+  updateCart(cart);
   console.log('현재 장바구니:', cart);
 }
