9940fbd3d2626a95fe4c05dd76fb1620cf5f9019
강현진 <66511872+Juferis@users.noreply.github.com>
2025-04-26 22:49:25 +0900

    fix: 불필요한 import 제거

--- src/main/java/com/grepp/spring/app/controller/web/cart/CartController.java
+++ src/main/java/com/grepp/spring/app/controller/web/cart/CartController.java
@@ -1,13 +1,10 @@
 package com.grepp.spring.app.controller.web.cart;
 
-import com.grepp.spring.app.model.cart.dto.CartResponse;
 import com.grepp.spring.app.model.cart.service.CartService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
